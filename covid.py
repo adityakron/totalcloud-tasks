83eae489cd7e00839be87e7c0ca2c5925a693439
aditya kolte <54710559+adityakron@users.noreply.github.com>
2021-04-16 23:23:39 +0530

    updated

--- covid.py
+++ covid.py
@@ -7,6 +7,8 @@ import urllib3
 response = requests.get("https://api.covid19india.org/v4/min/data.min.json")
 covid_data = json.loads(response.text)
 
+# All data store in covid_data variable
+
 print("State",'\t', "Active Cases")
 print("___________")
 for i,j in covid_data.items():
@@ -14,6 +16,8 @@ for i,j in covid_data.items():
   recover = j["total"]["recovered"]
   decease = j["total"]["deceased"]
 
+  # Active cases = confirmed - (recovered+deceased+others)
+  
   infected = recover+decease
   active = confirm-infected
   print(i,'\t', active)
